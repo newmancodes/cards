c70bc23f633a454eae1fe96ebb516d9e84b01593
Stephen Newman <steve@newman.digital>
2024-08-29 11:56:48 +0100

    Add documentation.

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,4 +1,11 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
+
+  @doc """
+    Returns a list of strings representing a deck of playing cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -8,27 +15,52 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    Takes a list of strings, representing a deck of playing cards, and returns a shuffled version of the playing cards
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines if a deck of cards, represented as a list of strings, and determines if a particular card is found within that deck
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck. The `hand_size` argument indicates how many cards should be in the hand
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> { hand, deck } = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
 
+  @doc """
+    Generates a new shuffled deck and deals a hand of size `hand_size`
+  """
   def create_hand(hand_size) do
     Cards.create_deck |> Cards.shuffle |> Cards.deal(hand_size)
   end
 
+  @doc """
+    Saves the supplied deck to the filename specified
+  """
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
 
+  @doc """
+    Loads the deck from the filename specified
+  """
   def load(filename) do
     case File.read(filename) do
       { :ok, binary } -> :erlang.binary_to_term(binary)
